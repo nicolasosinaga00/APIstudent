57e72cfe9b2bd04956bfc84803dd163e60ce5041
nicolasosinaga00 <a2019114771@estudiantes.upsa.edu.bo>
2020-08-22 12:00:33 -0400

    StudentController

---
+++ APIstudent/APIstudent/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace APIstudent.Models
+{
+    public class DataContext:DbContext 
+    {
+        public DataContext():base("DefaultConecction")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<APIstudent.Models.Student> Students { get; set; }
+    }
+}
